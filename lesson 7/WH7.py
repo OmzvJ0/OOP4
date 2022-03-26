3adab72024c15e38f5bd6092c53ea4e1b2f1e329
OmzvJ0 <omurzakov.janat@gmail.com>
2022-03-26 17:24:01 +0600

    HW5

--- lesson 7/WH7.py
+++ lesson 7/WH7.py
@@ -1,11 +1,11 @@
 def palindrome(string):
     n = str(string)
-    if n == n[::-1].startswith(n):
+    if n == n[::-1]:
         return True
     else:
         return False
 
 
-o = palindrome(123)
+o = palindrome(121)
 print(o)
 
